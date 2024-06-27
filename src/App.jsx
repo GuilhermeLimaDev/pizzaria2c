fcf93cd504073e155946f761d5a02507517693b8
GuilhermeLimaDev <guilhermeguiguigugu2509@gmail.com>
2024-06-27 09:28:16 -0300

    adicionando forms com hook-form

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Inicio from "./pages/inicio";
 import Listagem from "./pages/Listagem";
+import Basic from "./pages/Login";
 
 const App = () => {
   const [color] = useState("#eb8d04");
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Home />} />
       <Route path="/inicio" element={<Inicio />} />
       <Route path="/listagem" element={<Listagem />} />
+      <Route path="/Login" element={<Basic />} />
     </Routes>
   );
 };
